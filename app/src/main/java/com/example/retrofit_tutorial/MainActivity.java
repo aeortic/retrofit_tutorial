a3d3e1b00d45f97639d716b0effe39ea55b252c8
Chris Okerberg <seminative@gmail.com>
2020-01-22 14:38:12 -0500

    Simplified activity

--- app/src/main/java/com/example/retrofit_tutorial/MainActivity.java
+++ app/src/main/java/com/example/retrofit_tutorial/MainActivity.java
@@ -1,24 +1,19 @@
-package com.example.retrofit_tutorial;
+package com.example.retrofit_practice_1;
 
-import android.app.ProgressDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.retrofit_tutorial.R;
 import com.example.retrofit_tutorial.adapter.CustomAdapter;
 import com.example.retrofit_tutorial.model.RetroPhoto;
 import com.example.retrofit_tutorial.network.GetDataService;
 import com.example.retrofit_tutorial.network.RetrofitClientInstance;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -29,48 +24,36 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private CustomAdapter adapter;
     private RecyclerView recyclerView;
-    ProgressDialog progressDialog;
+    private ProgressBar progressBar;
+    private Call<List<RetroPhoto>> call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressDialog = new ProgressDialog(MainActivity.this);
-        progressDialog.setMessage("Loading...");
-        progressDialog.show();
+        progressBar = findViewById(R.id.indeterminateBar);
+        progressBar.setVisibility(View.VISIBLE);
 
-        /* Create handle for the RetrofitInstance interface */
+        /* Create handler for the Retrofit interface */
         GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
-        Call<List<RetroPhoto>> call = service.getAllPhotos();
+        call = service.getAllPhotos();
         call.enqueue(new Callback<List<RetroPhoto>>() {
             @Override
             public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
-                progressDialog.dismiss();
+                progressBar.setVisibility(View.INVISIBLE);
+                /* generate the list here */
                 generateDataList(response.body());
             }
 
             @Override
             public void onFailure(Call<List<RetroPhoto>> call, Throwable t) {
-                progressDialog.dismiss();
-                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                progressBar.setVisibility(View.INVISIBLE);
+                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT);
             }
         });
     }
 
-    /*Method to generate List of data using RecyclerView with custom adapter*/
     private void generateDataList(List<RetroPhoto> photoList) {
         recyclerView = findViewById(R.id.customRecyclerView);
         adapter = new CustomAdapter(this, photoList);
@@ -80,24 +63,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        super.onDestroy();
+        call.cancel();
     }
 }
