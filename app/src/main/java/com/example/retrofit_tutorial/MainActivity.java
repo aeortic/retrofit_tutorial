f3d46bf45fc104de9a1c704d89e604f183783518
Chris Okerberg <seminative@gmail.com>
2020-01-22 15:00:21 -0500

    Added comment to further explain location of fetch

--- app/src/main/java/com/example/retrofit_tutorial/MainActivity.java
+++ app/src/main/java/com/example/retrofit_tutorial/MainActivity.java
@@ -38,6 +38,15 @@ public class MainActivity extends AppCompatActivity {
         /* Create handler for the Retrofit interface */
         GetDataService service = RetrofitClientInstance.getRetrofitInstance().create(GetDataService.class);
         call = service.getAllPhotos();
+
+        /* In this naive example, the onCreate() lifecycle method is used for the sake of berevity.
+         * However, in a real-world application, the activity will likely not be the only screen
+         * visited by the user. In that case, onStart() would be a more natural fit. If the scope
+         * of the functionality allowed for even greater complexity, there would be a network cache
+         * that would persist the user's previous fetch, so that the functionality would not be
+         * hampered by poor network connectivity. Such decisions would require an analysis of the
+         * intended user experience required by the app.
+         */
         call.enqueue(new Callback<List<RetroPhoto>>() {
             @Override
             public void onResponse(Call<List<RetroPhoto>> call, Response<List<RetroPhoto>> response) {
